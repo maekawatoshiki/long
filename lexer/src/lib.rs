3eba6802230ef8ea502b32e4957defb4eaefc532
maekawatoshiki <konndennsa@gmail.com>
2021-11-03 23:25:54 +0900

    [lexer] Add 'SourceLexer'

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,26 +1,49 @@
-use std::path::PathBuf;
-
 pub mod token;
 
+extern crate anyhow;
 extern crate long_sourceloc as sourceloc;
+
+use anyhow::Result;
+use std::{fs::read_to_string, path::PathBuf};
+use token::Token;
 
 /// A lexical analyzer for a translation unit.
 pub struct Lexer {
     /// The file path of the source file the lexer originally reads.
     /// Set to `None` if the lexer does not read a file but a string.
     filepath: Option<PathBuf>,
+
+    /// The source lexers.
+    src_lexers: Vec<SourceLexer>,
+}
+
+/// A lexical analyzer for either a single source file or a string.
+struct SourceLexer {
+    /// The file path of the source file the lexer originally reads.
+    /// Set to `None` if the lexer does not read a file but a string.
+    filepath: Option<PathBuf>,
+
+    /// The source code the lexer reads.
+    source: String,
 }
 
 impl Lexer {
     /// Creates a new `Lexer`.
-    pub fn new(_source: String) -> Self {
-        Self { filepath: None }
+    pub fn new(source: impl Into<String>) -> Self {
+        Self {
+            filepath: None,
+            src_lexers: vec![SourceLexer::new(source.into())],
+        }
     }
 
     /// Creates a new `Lexer` for a file.
-    pub fn new_from_file(filepath: impl Into<PathBuf>) -> Self {
+    pub fn new_from_file<P>(filepath: P) -> Self
+    where
+        P: Into<PathBuf> + Clone,
+    {
         Self {
             filepath: Some(filepath.into()),
+            src_lexers: vec![],
         }
     }
 
@@ -28,10 +51,38 @@ impl Lexer {
     pub fn filepath(&self) -> Option<&PathBuf> {
         self.filepath.as_ref()
     }
+
+    /// Reads a token.
+    pub fn next(&mut self) -> Result<Token> {
+        todo!()
+    }
+}
+
+impl SourceLexer {
+    /// Creates a new `SourceLexer`.
+    pub fn new(source: String) -> Self {
+        Self {
+            filepath: None,
+            source,
+        }
+    }
+
+    /// Creates a new `SourceLexer` for a file.
+    pub fn new_from_file<P>(filepath: P) -> Result<Self>
+    where
+        P: Into<PathBuf> + Clone,
+    {
+        Ok(Self {
+            source: read_to_string(filepath.clone().into())?,
+            filepath: Some(filepath.into()),
+        })
+    }
 }
 
 #[test]
 fn test() {
     let l = Lexer::new_from_file("hello.c");
-    assert_eq!(l.filepath().unwrap().to_str().unwrap(), "hello.c")
+    assert_eq!(l.filepath().unwrap().to_str().unwrap(), "hello.c");
+    let l = Lexer::new("int main(){}");
+    assert!(l.filepath().is_none());
 }
