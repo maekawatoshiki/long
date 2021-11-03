6fc565a4b740b1c61a2af1a2043bdf1da95568e6
maekawatoshiki <konndennsa@gmail.com>
2021-11-03 22:40:50 +0900

    [lexer] Add 'Lexer'

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,3 +1,31 @@
+use std::path::PathBuf;
+
 pub mod token;
 
 extern crate long_sourceloc as sourceloc;
+
+/// A lexical analyzer for a translation unit.
+pub struct Lexer {
+    /// The file path of the source file the lexer originally reads.
+    filepath: PathBuf,
+}
+
+impl Lexer {
+    /// Creates a new `Lexer`.
+    pub fn new(filepath: impl Into<PathBuf>) -> Self {
+        Self {
+            filepath: filepath.into(),
+        }
+    }
+
+    /// Returns the file path.
+    pub fn filepath(&self) -> &PathBuf {
+        &self.filepath
+    }
+}
+
+#[test]
+fn test() {
+    let l = Lexer::new("hello.c");
+    assert_eq!(l.filepath().to_str().unwrap(), "hello.c")
+}
