39c4aa823330e3609b3b50708017f00d49fe306a
maekawatoshiki <konndennsa@gmail.com>
2021-11-03 22:58:17 +0900

    [lexer] 'Lexer' reads either a file or a string

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -7,25 +7,31 @@ extern crate long_sourceloc as sourceloc;
 /// A lexical analyzer for a translation unit.
 pub struct Lexer {
     /// The file path of the source file the lexer originally reads.
-    filepath: PathBuf,
+    /// Set to `None` if the lexer does not read a file but a string.
+    filepath: Option<PathBuf>,
 }
 
 impl Lexer {
     /// Creates a new `Lexer`.
-    pub fn new(filepath: impl Into<PathBuf>) -> Self {
+    pub fn new(_source: String) -> Self {
+        Self { filepath: None }
+    }
+
+    /// Creates a new `Lexer` for a file.
+    pub fn new_from_file(filepath: impl Into<PathBuf>) -> Self {
         Self {
-            filepath: filepath.into(),
+            filepath: Some(filepath.into()),
         }
     }
 
     /// Returns the file path.
-    pub fn filepath(&self) -> &PathBuf {
-        &self.filepath
+    pub fn filepath(&self) -> Option<&PathBuf> {
+        self.filepath.as_ref()
     }
 }
 
 #[test]
 fn test() {
-    let l = Lexer::new("hello.c");
-    assert_eq!(l.filepath().to_str().unwrap(), "hello.c")
+    let l = Lexer::new_from_file("hello.c");
+    assert_eq!(l.filepath().unwrap().to_str().unwrap(), "hello.c")
 }
