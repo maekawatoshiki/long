56117a2405851a3c7d30a675289353b3bb2d39b9
maekawatoshiki <konndennsa@gmail.com>
2021-11-03 22:24:46 +0900

    [lexer] Add 'TokenKind'

---
+++ lexer/src/token/kind.rs
@@ -0,0 +1,3 @@
+/// The kind of a token.
+#[derive(Debug, Clone)]
+pub enum TokenKind {}
--- lexer/src/token/mod.rs
+++ lexer/src/token/mod.rs
@@ -1,15 +1,29 @@
+pub mod kind;
+
+use kind::TokenKind;
 use long_sourceloc::SourceLoc;
 
 #[derive(Debug, Clone)]
 pub struct Token {
+    /// The kind of the token.
+    kind: TokenKind,
+
+    /// The source location of the token.
     loc: SourceLoc,
 }
 
 impl Token {
-    pub fn new(loc: SourceLoc) -> Self {
-        Self { loc }
+    /// Creates a new `Token`.
+    pub fn new(kind: TokenKind, loc: SourceLoc) -> Self {
+        Self { kind, loc }
     }
 
+    /// Returns the kind of the token.
+    pub fn kind(&self) -> &TokenKind {
+        &self.kind
+    }
+
+    /// Returns the source location of the token.
     pub fn loc(&self) -> &SourceLoc {
         &self.loc
     }
