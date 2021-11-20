bcd61aa3f04905c8334fc359f3f6cd8421dd26f9
maekawatoshiki <konndennsa@gmail.com>
2021-11-21 00:19:08 +0900

    [parser] Refine 'Parser::parse_decl_specifier()' and add tests for it

--- parser/src/decl.rs
+++ parser/src/decl.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use long_ast::node::ty::{Sign, Type};
 use long_ast::node::{decl::Decl, Located};
 use long_ast::token::kind::{KeywordKind, TokenKind};
+use long_ast::token::Token;
 
 impl<'a, L: LexerLike> Parser<'a, L> {
     /// Parses a declaration sequence.
@@ -31,8 +32,7 @@ impl<'a, L: LexerLike> Parser<'a, L> {
 
     /// Parses declaration specifiers.
     /// <https://timsong-cpp.github.io/cppwp/n3337/dcl.spec#nt:decl-specifier-seq>
-    // Add tests for this.
-    fn parse_decl_specifier(&mut self) -> Result<Type> {
+    fn parse_decl_specifier(&mut self) -> Result<Located<Type>> {
         #[derive(PartialEq, Debug, Clone)]
         enum Size {
             Short,
@@ -48,6 +48,7 @@ impl<'a, L: LexerLike> Parser<'a, L> {
             Char,
             Char16,
             Char32,
+            WChar,
             Int,
             Float,
             Double,
@@ -55,9 +56,29 @@ impl<'a, L: LexerLike> Parser<'a, L> {
 
         let mut ty = None;
         let mut sign = None;
+        let mut loc = None;
+        let mut size = Size::Unspecified;
+
+        fn err(tok: Token) -> Result<()> {
+            Err(Error::Message(
+                format!("Cannot specify '{}'", tok.kind().to_string()),
+                *tok.loc(),
+            )
+            .into())
+        }
 
         loop {
-            let tok = self.lexer.peek()?.ok_or(Error::UnexpectedEof)?;
+            let tok = match self.lexer.next()? {
+                Some(tok) => tok,
+                None => break,
+            };
+
+            if loc.is_none() {
+                loc = Some(*tok.loc());
+            }
+
+            // TODO: Support more types: struct, union, enum, class, user defined types...
+
             match tok.kind() {
                 TokenKind::Keyword(
                     KeywordKind::Void
@@ -68,59 +89,105 @@ impl<'a, L: LexerLike> Parser<'a, L> {
                     | KeywordKind::Int
                     | KeywordKind::Float
                     | KeywordKind::Double,
-                ) if ty.is_some() => {
-                    return Err(Error::Message(
-                        format!("Cannot specify '{}'", tok.kind().to_string()),
-                        *tok.loc(),
-                    )
-                    .into())
-                }
+                ) if ty.is_some() => err(tok)?,
                 TokenKind::Keyword(KeywordKind::Void) => ty = Some(PrimitiveType::Void),
                 TokenKind::Keyword(KeywordKind::Bool) => ty = Some(PrimitiveType::Bool),
                 TokenKind::Keyword(KeywordKind::Char) => ty = Some(PrimitiveType::Char),
                 TokenKind::Keyword(KeywordKind::Char16T) => ty = Some(PrimitiveType::Char16),
                 TokenKind::Keyword(KeywordKind::Char32T) => ty = Some(PrimitiveType::Char32),
+                TokenKind::Keyword(KeywordKind::WcharT) => ty = Some(PrimitiveType::WChar),
                 TokenKind::Keyword(KeywordKind::Int) => ty = Some(PrimitiveType::Int),
                 TokenKind::Keyword(KeywordKind::Float) => ty = Some(PrimitiveType::Float),
                 TokenKind::Keyword(KeywordKind::Double) => ty = Some(PrimitiveType::Double),
+                TokenKind::Keyword(KeywordKind::Short) if size == Size::Unspecified => {
+                    size = Size::Short
+                }
+                TokenKind::Keyword(KeywordKind::Short) => err(tok)?,
+                TokenKind::Keyword(KeywordKind::Long) if size == Size::Unspecified => {
+                    size = Size::Long
+                }
+                TokenKind::Keyword(KeywordKind::Long) if size == Size::Long => {
+                    size = Size::LongLong;
+                }
+                TokenKind::Keyword(KeywordKind::Long) if size == Size::LongLong => {
+                    err(tok)?;
+                }
                 TokenKind::Keyword(KeywordKind::Signed) if sign == Some(Sign::Signed) => {
                     // TODO: Had better raise a warning message.
                 }
                 TokenKind::Keyword(KeywordKind::Signed) if sign == Some(Sign::Unsigned) => {
-                    return Err(Error::Message(
-                        format!("Cannot specify '{}'", tok.kind().to_string()),
-                        *tok.loc(),
-                    )
-                    .into())
+                    err(tok)?
                 }
                 TokenKind::Keyword(KeywordKind::Unsigned) if sign == Some(Sign::Signed) => {
-                    return Err(Error::Message(
-                        format!("Cannot specify '{}'", tok.kind().to_string()),
-                        *tok.loc(),
-                    )
-                    .into())
+                    err(tok)?
                 }
                 TokenKind::Keyword(KeywordKind::Signed) => sign = Some(Sign::Signed),
                 TokenKind::Keyword(KeywordKind::Unsigned) => sign = Some(Sign::Unsigned),
-                _ => break,
+                _ => {
+                    self.lexer.unget(tok);
+                    break;
+                }
             }
-            self.lexer.next()?;
         }
 
+        let loc = match loc {
+            Some(loc) => loc,
+            None => return Err(Error::UnexpectedEof.into()),
+        };
         let sign = sign.unwrap_or(Sign::Signed);
 
-        match ty {
-            Some(PrimitiveType::Void) => return Ok(Type::Void),
-            Some(PrimitiveType::Bool) => return Ok(Type::Bool),
-            Some(PrimitiveType::Char) => return Ok(Type::Char(sign)),
-            Some(PrimitiveType::Char16) => return Ok(Type::Char16(sign)),
-            Some(PrimitiveType::Char32) => return Ok(Type::Char32(sign)),
-            Some(PrimitiveType::Int) => return Ok(Type::Int(sign)),
-            Some(PrimitiveType::Float) => return Ok(Type::Float),
-            Some(PrimitiveType::Double) => return Ok(Type::Double),
-            None => {}
-        }
+        let ty = match ty {
+            Some(PrimitiveType::Void) => Type::Void,
+            Some(PrimitiveType::Bool) => Type::Bool,
+            Some(PrimitiveType::Char) => Type::Char(sign),
+            Some(PrimitiveType::Char16) => Type::Char16(sign),
+            Some(PrimitiveType::Char32) => Type::Char32(sign),
+            Some(PrimitiveType::WChar) => Type::WChar(sign),
+            Some(PrimitiveType::Int) if size == Size::Short => Type::Short(sign),
+            Some(PrimitiveType::Int) if size == Size::Long => Type::Long(sign),
+            Some(PrimitiveType::Int) if size == Size::LongLong => Type::LongLong(sign),
+            Some(PrimitiveType::Int) => Type::Int(sign),
+            Some(PrimitiveType::Float) => Type::Float,
+            Some(PrimitiveType::Double) => Type::Double,
+            None => match size {
+                Size::Short => Type::Short(sign),
+                Size::Long => Type::Long(sign),
+                Size::LongLong => Type::LongLong(sign),
+                Size::Unspecified => Type::Int(sign),
+            },
+        };
 
-        todo!()
+        Ok(Located::new(ty, loc))
     }
 }
+
+macro_rules! decl_specifier_test {
+    ($name:ident, $src:expr) => {
+        #[test]
+        fn $name() {
+            use crate::lexer::Lexer;
+            let mut lexer = Lexer::new($src);
+            let mut parser = Parser::new(&mut lexer);
+            insta::assert_debug_snapshot!(parser.parse_decl_specifier());
+        }
+    };
+}
+
+decl_specifier_test!(decl_spec_empty, "");
+decl_specifier_test!(decl_spec_bool, "bool");
+decl_specifier_test!(decl_spec_char, "char");
+decl_specifier_test!(decl_spec_char16, "char16_t");
+decl_specifier_test!(decl_spec_char32, "char32_t");
+decl_specifier_test!(decl_spec_unsigned_char, "unsigned char");
+decl_specifier_test!(decl_spec_unsigned_char_2, "char unsigned");
+decl_specifier_test!(decl_spec_wchar, "wchar_t");
+decl_specifier_test!(decl_spec_short, "short");
+decl_specifier_test!(decl_spec_int, "int");
+decl_specifier_test!(decl_spec_long, "long");
+decl_specifier_test!(decl_spec_long_long, "long long");
+decl_specifier_test!(decl_spec_long_long_long, "long long long");
+decl_specifier_test!(decl_spec_int_long, "int long");
+decl_specifier_test!(decl_spec_long_int, "long int");
+decl_specifier_test!(decl_spec_unsigned_long_int, "unsigned long int");
+decl_specifier_test!(decl_spec_float, "float");
+decl_specifier_test!(decl_spec_double, "double");
