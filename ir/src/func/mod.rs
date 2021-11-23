0cc517948d0c862a05c90549bbdfc0ad5aacb0a1
maekawatoshiki <konndennsa@gmail.com>
2021-11-23 17:55:36 +0900

    [ir] Add 'FunctionSignature' 'Module'

--- ir/src/func/mod.rs
+++ ir/src/func/mod.rs
@@ -1,13 +1,21 @@
 use crate::{name::NameId, ty::Type};
 
 /// A function.
+#[derive(Debug, Clone)]
 pub struct Function {
     pub name: NameId,
+    pub sig: FunctionSignature,
+}
+
+/// A function signature (i.e. the return type and parameters of a function).
+#[derive(Debug, Clone)]
+pub struct FunctionSignature {
     pub ret: Type,
-    pub params: Vec<Param>,
+    pub params: Vec<Type>,
 }
 
 /// A parameter of a function.
+#[derive(Debug, Clone)]
 pub struct Param {
     pub name: NameId,
     pub ty: Type,
--- ir/src/lib.rs
+++ ir/src/lib.rs
@@ -1,3 +1,14 @@
+use func::Function;
+use id_arena::Arena;
+use name::NameArena;
+
 pub mod func;
 pub mod name;
 pub mod ty;
+
+/// A translation unit represented in the IR.
+#[derive(Debug, Clone)]
+pub struct Module {
+    pub name_arena: NameArena,
+    pub func_arena: Arena<Function>,
+}
