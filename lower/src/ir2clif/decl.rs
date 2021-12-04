9f2d94a860f36a40e1534f55771f9b1c0b8f5e9f
maekawatoshiki <konndennsa@gmail.com>
2021-12-04 14:31:28 +0900

    [lower] Add tests

--- lower/src/ir2clif/decl.rs
+++ lower/src/ir2clif/decl.rs
@@ -6,6 +6,7 @@ use cranelift::{
     frontend::{FunctionBuilder, FunctionBuilderContext},
     prelude::{AbiParam, Signature, StackSlotData, StackSlotKind},
 };
+use cranelift_codegen::ir::Function;
 use cranelift_codegen::{
     binemit::{NullStackMapSink, NullTrapSink},
     ir::StackSlot,
@@ -48,7 +49,30 @@ pub fn lower_simple_decl(ctx: &mut LowerCtx, decl: &SimpleDecl) -> Result<()> {
 }
 
 pub fn lower_funcdef(ctx: &mut LowerCtx, funcdef: &FuncDef<'_>) -> Result<()> {
+    lower_funcdef_sub(ctx, funcdef)?;
+
+    let name = mangle_name(funcdef.name);
+    let func_id = ctx.module.declare_function(
+        name.as_str(),
+        Linkage::Export,
+        &ctx.clif_ctx.func.signature,
+    )?;
+    ctx.module.define_function(
+        func_id,
+        &mut ctx.clif_ctx,
+        &mut NullTrapSink {},
+        &mut NullStackMapSink {},
+    )?;
+    ctx.module.clear_context(&mut ctx.clif_ctx);
+
+    Ok(())
+}
+
+pub fn lower_funcdef_sub<'a>(ctx: &'a mut LowerCtx, funcdef: &FuncDef<'_>) -> Result<&'a Function> {
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
     let mut sig = Signature::new(CallConv::SystemV);
+    #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+    let mut sig = Signature::new(CallConv::AppleAarch64);
     sig.returns
         .push(AbiParam::new(convert_type(funcdef.sig.ret)));
     ctx.clif_ctx.func.signature = sig;
@@ -78,19 +102,44 @@ pub fn lower_funcdef(ctx: &mut LowerCtx, funcdef: &FuncDef<'_>) -> Result<()> {
     }
     builder.finalize();
 
-    let name = mangle_name(funcdef.name);
-    let func_id = ctx.module.declare_function(
-        name.as_str(),
-        Linkage::Export,
-        &ctx.clif_ctx.func.signature,
-    )?;
-    ctx.module.define_function(
-        func_id,
-        &mut ctx.clif_ctx,
-        &mut NullTrapSink {},
-        &mut NullStackMapSink {},
-    )?;
-    ctx.module.clear_context(&mut ctx.clif_ctx);
-
-    Ok(())
+    Ok(&ctx.clif_ctx.func)
 }
+
+macro_rules! lower_func {
+    ($name:ident, $src:expr) => {
+        #[test]
+        fn $name() {
+            use crate::ast2ir;
+            use long_ast::node::Located;
+            use long_ir::decl::Decl;
+            use long_ir::Context as IrContext;
+            use long_parser::lexer::Lexer;
+            use long_parser::Parser;
+            let Located { inner, .. } = Parser::new(&mut Lexer::new($src))
+                .parse_program()
+                .unwrap()
+                .into_iter()
+                .next()
+                .unwrap();
+            let ctx = IrContext::new();
+            let mut ctx = ast2ir::LowerCtx::new(&ctx);
+            let decl = ast2ir::lower_decl(&mut ctx, &inner).unwrap();
+            let mut ctx = LowerCtx::new();
+            let clif_func = lower_funcdef_sub(
+                &mut ctx,
+                match decl {
+                    Decl::FuncDef(funcdef) => funcdef,
+                    _ => todo!(),
+                },
+            )
+            .unwrap();
+            insta::assert_display_snapshot!(clif_func.display());
+        }
+    };
+}
+
+lower_func!(arith, "int f() { return 1 + 2 - 3 * 4 / 5; }");
+lower_func!(
+    arith2,
+    "int f() { int i; i = 1; i = i + 2; i = i - 3; i = i * 4; i = i / 5; return i; }"
+);
